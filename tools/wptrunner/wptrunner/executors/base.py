a37cba89e356473f346567e1402d6da0a4a1d9c3
Valerie Young <spectranaut@igalia.com>
2024-04-30 17:18:04 -0700

    Create new test type for accessibility API testing

--- tools/wptrunner/wptrunner/executors/base.py
+++ tools/wptrunner/wptrunner/executors/base.py
@@ -39,7 +39,7 @@ def executor_kwargs(test_type, test_environment, run_info_data, subsuite, **kwar
         executor_kwargs["screenshot_cache"] = screenshot_cache
         executor_kwargs["reftest_screenshot"] = kwargs["reftest_screenshot"]
 
-    if test_type == "wdspec":
+    if test_type in ("wdspec", "aamspec"):
         executor_kwargs["binary"] = kwargs["binary"]
         executor_kwargs["binary_args"] = kwargs["binary_args"].copy()
         executor_kwargs["webdriver_binary"] = kwargs["webdriver_binary"]
@@ -703,7 +703,7 @@ class WdspecExecutor(TestExecutor):
 
         success, data = WdspecRun(self.do_wdspec,
                                   test.abs_path,
-                                  timeout).run()
+                                  timeout).run() # 
 
         if success:
             return self.convert_result(test, data)
